95c9dc14b3b1589b48613d0250551857784e274c
Jappi-Boss <ababemobile@gmail.com>
2026-08-30 13:42:11 +0300

    Use stable hosted brand imagery

---
+++ src/lib/site-images.ts
@@ -0,0 +1,7 @@
+const assetBase =
+  "https://raw.githubusercontent.com/Jappi-Boss/Tona_coffee/46644382d0a190e7c0ffa62b0f6bd930ee894b76/src/assets";
+
+export const heroCeremonyImage = `${assetBase}/hero-ceremony.jpg`;
+export const beansImage = `${assetBase}/beans.jpg`;
+export const businessImage = `${assetBase}/business.jpg`;
+export const eventImage = `${assetBase}/event.jpg`;
