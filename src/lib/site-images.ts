85cd5146265a0e3f49593cb0daae0aea3664dcd6
Jappi-Boss <ababemobile@gmail.com>
2026-09-03 16:56:57 +0300

    Use new Tona product image in homepage hero

--- src/lib/site-images.ts
+++ src/lib/site-images.ts
@@ -1,7 +1,9 @@
+import tonaHomeHeroImage from "@/assets/tona-home-hero.jpg";
+
 const assetBase =
   "https://raw.githubusercontent.com/Jappi-Boss/Tona_coffee/46644382d0a190e7c0ffa62b0f6bd930ee894b76/src/assets";
 
-export const heroCeremonyImage = `${assetBase}/hero-ceremony.jpg`;
+export const heroCeremonyImage = tonaHomeHeroImage;
 export const beansImage = `${assetBase}/beans.jpg`;
 export const businessImage = `${assetBase}/business.jpg`;
 export const eventImage = `${assetBase}/event.jpg`;
