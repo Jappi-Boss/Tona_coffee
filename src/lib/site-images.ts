b294e1a22b3d71632ff9be96081551d17d645fee
Jappi-Boss <ababemobile@gmail.com>
2026-09-05 13:50:04 +0300

    Use approved Tona product image in homepage partners section

--- src/lib/site-images.ts
+++ src/lib/site-images.ts
@@ -1,8 +1,10 @@
+import { HOME_PARTNERS_IMAGE } from "./home-partners-image";
+
 const assetBase =
   "https://raw.githubusercontent.com/Jappi-Boss/Tona_coffee/46644382d0a190e7c0ffa62b0f6bd930ee894b76/src/assets";
 
 export const heroCeremonyImage =
   "https://res.cloudinary.com/tkiiddxu/image/upload/v1788585734/tona-home-hero-approved_1.jpg";
-export const beansImage = `${assetBase}/beans.jpg`;
+export const beansImage = HOME_PARTNERS_IMAGE;
 export const businessImage = `${assetBase}/business.jpg`;
 export const eventImage = `${assetBase}/event.jpg`;
