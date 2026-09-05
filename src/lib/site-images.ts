48be7f34ef9b165d0cd3984a1c25ead655c447f1
Jappi-Boss <ababemobile@gmail.com>
2026-09-05 08:24:28 +0300

    Use approved Cloudinary hero image

--- src/lib/site-images.ts
+++ src/lib/site-images.ts
@@ -1,9 +1,8 @@
-import tonaHomeHeroImage from "@/assets/tona-home-hero-hq.jpeg";
-
 const assetBase =
   "https://raw.githubusercontent.com/Jappi-Boss/Tona_coffee/46644382d0a190e7c0ffa62b0f6bd930ee894b76/src/assets";
 
-export const heroCeremonyImage = tonaHomeHeroImage;
+export const heroCeremonyImage =
+  "https://res.cloudinary.com/tkiiddxu/image/upload/v1788585734/tona-home-hero-approved_1.jpg";
 export const beansImage = `${assetBase}/beans.jpg`;
 export const businessImage = `${assetBase}/business.jpg`;
 export const eventImage = `${assetBase}/event.jpg`;
