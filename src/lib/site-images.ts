3633389812a8b9b78b8917aea1029202fbf138bd
Jappi-Boss <ababemobile@gmail.com>
2026-09-10 23:35:45 +0300

    Use preview hero image on main homepage

--- src/lib/site-images.ts
+++ src/lib/site-images.ts
@@ -4,7 +4,7 @@ const assetBase =
   "https://raw.githubusercontent.com/Jappi-Boss/Tona_coffee/46644382d0a190e7c0ffa62b0f6bd930ee894b76/src/assets";
 
 export const heroCeremonyImage =
-  "https://res.cloudinary.com/tkiiddxu/image/upload/v1788585734/tona-home-hero-approved_1.jpg";
+  "https://tona-coffee-two.vercel.app/framer-hero-illustration.png";
 export const beansImage = HOME_PARTNERS_IMAGE;
 export const businessImage =
   "https://res.cloudinary.com/tkiiddxu/image/upload/v1788710155/tona-business-partnership-approved.webp";
