273ed1c854a0a8fe2083d573000eb4355f5f0745
Jappi-Boss <ababemobile@gmail.com>
2026-09-11 13:35:16 +0300

    Improve event product image quality

--- src/lib/events-product-image.ts
+++ src/lib/events-product-image.ts
@@ -1,7 +1,1 @@
-import part01 from "@/assets/events-product-final/part01.txt?raw";
-import part02 from "@/assets/events-product-final/part02.txt?raw";
-import part03 from "@/assets/events-product-final/part03.txt?raw";
-import part04 from "@/assets/events-product-final/part04.txt?raw";
-
-export const EVENTS_PRODUCT_IMAGE =
-  `data:image/jpeg;base64,${part01}${part02}${part03}${part04}`;
+export const EVENTS_PRODUCT_IMAGE = "/events-product-display-hq.jpg";
